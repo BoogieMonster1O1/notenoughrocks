2d06f54e6805e1444a777d1d61d0f2bbcc678109
BoogieMonster1O1 <harrypotter.starwars.shrish@gmail.com>
2020-05-19 20:29:41 +0530

    Tile Superclass

---
+++ src/main/java/io/github/boogiemonster1o1/notenoughrocks/blocks/TileBlock.java
@@ -0,0 +1,31 @@
+package io.github.boogiemonster1o1.notenoughrocks.blocks;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockRenderType;
+import net.minecraft.block.BlockState;
+import net.minecraft.entity.EntityContext;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.world.BlockView;
+
+import static net.minecraft.block.BlockRenderType.MODEL;
+import static net.minecraft.util.shape.VoxelShapes.cuboid;
+
+public class TileBlock extends Block {
+    public TileBlock(Settings settings) {
+        super(settings);
+    }
+
+    public static Identifier TILES = new Identifier("minecraft","tiles");
+
+    @Override
+    public BlockRenderType getRenderType(BlockState blockState) {
+        return MODEL;
+    }
+
+    @Override
+    public VoxelShape getOutlineShape(BlockState blockState, BlockView blockView, BlockPos blockPos, EntityContext entityContext) {
+        return cuboid(0,0,0,1,0.0625F,1);
+    }
+}
