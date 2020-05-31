095ec6a0f3dc96f0923cd46ba607f3ba4e816b18
BoogieMonster1O1 <harrypotter.starwars.shrish@gmail.com>
2020-05-31 17:10:46 +0530

    Modified the mixin

--- src/main/java/io/github/boogiemonster1o1/notenoughrocks/mixin/FluidBlockMixin.java
+++ src/main/java/io/github/boogiemonster1o1/notenoughrocks/mixin/FluidBlockMixin.java
@@ -17,9 +17,6 @@ import static io.github.boogiemonster1o1.notenoughrocks.Elements.BlockS.LIMESTON
 
 @Mixin(FluidBlock.class)
 public class FluidBlockMixin  {
-    @Shadow
-    private void playExtinguishSound(IWorld world, BlockPos pos){}
-
     @Inject(at=@At(value = "INVOKE",ordinal = 1,target = "Lnet/minecraft/block/FluidBlock;playExtinguishSound(Lnet/minecraft/world/IWorld;Lnet/minecraft/util/math/BlockPos;)V"),method="receiveNeighborFluids")
     public void shouldHaveBeenCalledBlendinAndNotMixin(World world, BlockPos pos, BlockState state, CallbackInfoReturnable<Boolean> cir){
         int randomNumberGeneratedFromJavaDotUtilDotRandom = new Random().nextInt(10);
