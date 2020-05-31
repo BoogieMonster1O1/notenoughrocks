adc47fc915f140d6521360ab4ed53d14726b83df
BoogieMonster1O1 <harrypotter.starwars.shrish@gmail.com>
2020-05-31 21:40:38 +0530

    Fixed tiles entirely

--- src/main/java/io/github/boogiemonster1o1/notenoughrocks/blocks/TileBlock.java
+++ src/main/java/io/github/boogiemonster1o1/notenoughrocks/blocks/TileBlock.java
@@ -1,48 +1,58 @@
 package io.github.boogiemonster1o1.notenoughrocks.blocks;
 
 import net.minecraft.block.Block;
-import net.minecraft.block.BlockRenderType;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
 import net.minecraft.block.Waterloggable;
 import net.minecraft.entity.EntityContext;
 import net.minecraft.fluid.FluidState;
 import net.minecraft.fluid.Fluids;
 import net.minecraft.state.property.BooleanProperty;
 import net.minecraft.state.property.Properties;
+import net.minecraft.tag.BlockTags;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.world.BlockView;
+import net.minecraft.world.IWorld;
+import net.minecraft.world.WorldView;
 
-import static net.minecraft.block.BlockRenderType.MODEL;
+import static net.minecraft.block.Blocks.AIR;
 import static net.minecraft.state.StateManager.Builder;
 import static net.minecraft.util.shape.VoxelShapes.cuboid;
 
 public class TileBlock extends Block implements Waterloggable {
     public TileBlock(Settings settings) {
         super(settings);
+        this.setDefaultState(this.stateManager.getDefaultState().with(WATERLOGGED, false));
     }
 
     public static Identifier TILES = new Identifier("minecraft","tiles");
 
     public static final BooleanProperty WATERLOGGED = Properties.WATERLOGGED;
-
-    @Override
-    public BlockRenderType getRenderType(BlockState blockState) {
-        return MODEL;
-    }
-
     @Override
     public VoxelShape getOutlineShape(BlockState blockState, BlockView blockView, BlockPos blockPos, EntityContext entityContext) {
-        return cuboid(0,0,0,1,0.125F,1);
+        return cuboid(0.0F,0,0,1,0.0625F,1.0F);
     }
     @Override
     public FluidState getFluidState(BlockState state) {
         return state.get(WATERLOGGED) ? Fluids.WATER.getStill(false) : super.getFluidState(state);
     }
-
     @Override
-    protected void appendProperties(Builder<Block, BlockState> stateManager) {
+    public void appendProperties(Builder<Block, BlockState> stateManager) {
         stateManager.add(WATERLOGGED);
     }
+
+    @Override
+    public boolean canPlaceAt(BlockState state, WorldView world, BlockPos pos) {
+        if(BlockTags.getContainer().getOrCreate(new Identifier("minecraft","columns")).contains(world.getBlockState(pos.down()).getBlock()) || BlockTags.getContainer().getOrCreate(new Identifier("minecraft","walls")).contains(world.getBlockState(pos.down()).getBlock())) return super.canPlaceAt(state,world,pos);
+        return sideCoversSmallSquare(world, pos.down(), Direction.UP);
+    }
+
+    @Override
+    public BlockState getStateForNeighborUpdate(BlockState state, Direction facing, BlockState neighborState, IWorld world, BlockPos pos, BlockPos neighborPos) {
+        return !this.canPlaceAt(state, world, pos) ? Blocks.AIR.getDefaultState() : super.getStateForNeighborUpdate(state, facing, neighborState, world, pos, neighborPos);
+    }
+
 }
