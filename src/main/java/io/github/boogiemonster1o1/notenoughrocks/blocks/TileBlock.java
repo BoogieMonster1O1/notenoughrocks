aaa1bf932c477c03ef4f69aa08a75aa7258b1802
BoogieMonster1O1 <harrypotter.starwars.shrish@gmail.com>
2020-05-31 17:21:02 +0530

    Waterloggable tiles:

--- src/main/java/io/github/boogiemonster1o1/notenoughrocks/blocks/TileBlock.java
+++ src/main/java/io/github/boogiemonster1o1/notenoughrocks/blocks/TileBlock.java
@@ -3,7 +3,13 @@ package io.github.boogiemonster1o1.notenoughrocks.blocks;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockRenderType;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.Waterloggable;
 import net.minecraft.entity.EntityContext;
+import net.minecraft.fluid.FluidState;
+import net.minecraft.fluid.Fluids;
+import net.minecraft.state.StateManager;
+import net.minecraft.state.property.BooleanProperty;
+import net.minecraft.state.property.Properties;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.shape.VoxelShape;
@@ -12,12 +18,14 @@ import net.minecraft.world.BlockView;
 import static net.minecraft.block.BlockRenderType.MODEL;
 import static net.minecraft.util.shape.VoxelShapes.cuboid;
 
-public class TileBlock extends Block {
+public class TileBlock extends Block implements Waterloggable {
     public TileBlock(Settings settings) {
         super(settings);
     }
 
     public static Identifier TILES = new Identifier("minecraft","tiles");
+
+    public static final BooleanProperty WATERLOGGED = Properties.WATERLOGGED;
 
     @Override
     public BlockRenderType getRenderType(BlockState blockState) {
@@ -27,5 +35,14 @@ public class TileBlock extends Block {
     @Override
     public VoxelShape getOutlineShape(BlockState blockState, BlockView blockView, BlockPos blockPos, EntityContext entityContext) {
         return cuboid(0,0,0,1,0.125F,1);
+    }
+    @Override
+    public FluidState getFluidState(BlockState state) {
+        return state.get(WATERLOGGED) ? Fluids.WATER.getStill(false) : super.getFluidState(state);
+    }
+
+    @Override
+    protected void appendProperties(StateManager.Builder<Block, BlockState> stateManager) {
+        stateManager.add(WATERLOGGED);
     }
 }
