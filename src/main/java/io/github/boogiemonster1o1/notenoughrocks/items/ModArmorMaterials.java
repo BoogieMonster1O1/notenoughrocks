84bb4bef9dc495445a50f553cd762e6980b5d6d7
BoogieMonster1O1 <harrypotter.starwars.shrish@gmail.com>
2020-06-03 18:51:38 +0530

    Fixed armor item models

--- src/main/java/io/github/boogiemonster1o1/notenoughrocks/items/ModArmorMaterials.java
+++ src/main/java/io/github/boogiemonster1o1/notenoughrocks/items/ModArmorMaterials.java
@@ -6,31 +6,33 @@ import net.minecraft.recipe.Ingredient;
 import net.minecraft.sound.SoundEvent;
 import net.minecraft.util.Lazy;
 
+import java.util.function.Supplier;
+
 import static io.github.boogiemonster1o1.notenoughrocks.Elements.ItemS.LIMESTONE_BLOCK_ITEM;
 import static net.minecraft.item.Items.STONE;
 import static net.minecraft.recipe.Ingredient.ofItems;
 import static net.minecraft.sound.SoundEvents.ITEM_ARMOR_EQUIP_CHAIN;
 
 public enum ModArmorMaterials implements ArmorMaterial {
-    HEAVY_ROCK("heavy_rock", 10, new int[]{}, 5, ITEM_ARMOR_EQUIP_CHAIN, 1.0F, ofItems(LIMESTONE_BLOCK_ITEM, STONE));
+    HEAVY_ROCK("heavy_rock", 10, new int[]{2, 5, 6, 2}, 5, ITEM_ARMOR_EQUIP_CHAIN, 1.0F, ()-> ofItems(LIMESTONE_BLOCK_ITEM, STONE));
 
     public static final int[] BASE_DURABILITY = new int[]{13, 15, 16, 11};
     private final String name;
     private final int durabilityMultiplier;
-    private final int[] armorValues;
+    private final int[] protectionAmounts;
     private final int enchantability;
     private final SoundEvent equipSound;
     private final float toughness;
     private final Lazy<Ingredient> repairIngredient;
 
-    ModArmorMaterials(String name, int durabilityMultiplier, int[] armorValueArr, int enchantability, SoundEvent soundEvent, float toughness, Ingredient repair) {
+    ModArmorMaterials(String name, int durabilityMultiplier, int[] protectionAmounts, int enchantability, SoundEvent equipSound, float toughness, Supplier<Ingredient> repairIngredient) {
         this.name = name;
         this.durabilityMultiplier = durabilityMultiplier;
-        this.armorValues = armorValueArr;
+        this.protectionAmounts = protectionAmounts;
         this.enchantability = enchantability;
-        this.equipSound = soundEvent;
+        this.equipSound = equipSound;
         this.toughness = toughness;
-        this.repairIngredient = new Lazy<>(() -> repair);
+        this.repairIngredient = new Lazy<>(repairIngredient);
     }
 
     @Override
@@ -40,7 +42,7 @@ public enum ModArmorMaterials implements ArmorMaterial {
 
     @Override
     public int getProtectionAmount(EquipmentSlot slot) {
-        return this.armorValues[slot.getEntitySlotId()];
+        return this.protectionAmounts[slot.getEntitySlotId()];
     }
 
     @Override
