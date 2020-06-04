71caaee6e5d1742e4498a965fa6d7cd753be534c
BoogieMonster1O1 <harrypotter.starwars.shrish@gmail.com>
2020-06-04 09:46:44 +0530

    combat de-nerfed

--- src/main/java/io/github/boogiemonster1o1/notenoughrocks/items/FineRockDustItem.java
+++ src/main/java/io/github/boogiemonster1o1/notenoughrocks/items/FineRockDustItem.java
@@ -3,22 +3,50 @@ package io.github.boogiemonster1o1.notenoughrocks.items;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.item.TooltipContext;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.text.Text;
 import net.minecraft.text.TranslatableText;
+import net.minecraft.util.Hand;
 import net.minecraft.world.World;
 
 import java.util.List;
 
+import static io.github.boogiemonster1o1.notenoughrocks.Elements.ItemS.*;
+import static net.minecraft.entity.EquipmentSlot.*;
+
 public class FineRockDustItem extends Item {
     public FineRockDustItem(Settings settings) {
-        super(settings);
+        super(settings.maxCount(1));
     }
 
     @Environment(EnvType.CLIENT)
     @Override
     public void appendTooltip(ItemStack stack, World world, List<Text> tooltip, TooltipContext context) {
        tooltip.add(new TranslatableText("item.notenoughrocks.fine_dust.use_dust_tooltip"));
+    }
+
+    @Override
+    public boolean useOnEntity(ItemStack stack, PlayerEntity user, LivingEntity entity, Hand hand) {
+        Item headArmor = user.getEquippedStack(HEAD).getItem();
+        Item chestArmor = user.getEquippedStack(CHEST).getItem();
+        Item legsArmor = user.getEquippedStack(LEGS).getItem();
+        Item feetArmor = user.getEquippedStack(FEET).getItem();
+        if(headArmor == HEAVY_ROCK_HELMET || chestArmor == HEAVY_ROCK_CHESTPLATE || legsArmor == HEAVY_ROCK_LEGGINGS || feetArmor == HEAVY_ROCK_BOOTS){
+            if(!entity.hasStatusEffect(StatusEffects.BLINDNESS)){
+                entity.addStatusEffect(new StatusEffectInstance(StatusEffects.BLINDNESS,4));
+                stack.decrement(1);
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public boolean hasEnchantmentGlint(ItemStack stack) {
+        return true;
     }
 }
