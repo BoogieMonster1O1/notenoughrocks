9a562a6cf0af3d570af7e949a16e7c63f249f92a
BoogieMonster1O1 <harrypotter.starwars.shrish@gmail.com>
2020-06-04 10:25:36 +0530

    Change fine dust

--- src/main/java/io/github/boogiemonster1o1/notenoughrocks/items/FineRockDustItem.java
+++ src/main/java/io/github/boogiemonster1o1/notenoughrocks/items/FineRockDustItem.java
@@ -9,19 +9,21 @@ import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.sound.SoundEvents;
 import net.minecraft.text.Text;
 import net.minecraft.text.TranslatableText;
 import net.minecraft.util.Hand;
 import net.minecraft.world.World;
 
 import java.util.List;
+import java.util.Random;
 
 import static io.github.boogiemonster1o1.notenoughrocks.Elements.ItemS.*;
 import static net.minecraft.entity.EquipmentSlot.*;
 
 public class FineRockDustItem extends Item {
     public FineRockDustItem(Settings settings) {
-        super(settings.maxCount(1));
+        super(settings.maxDamage(1));
     }
 
     @Environment(EnvType.CLIENT)
@@ -37,9 +39,10 @@ public class FineRockDustItem extends Item {
         Item legsArmor = user.getEquippedStack(LEGS).getItem();
         Item feetArmor = user.getEquippedStack(FEET).getItem();
         if(headArmor == HEAVY_ROCK_HELMET || chestArmor == HEAVY_ROCK_CHESTPLATE || legsArmor == HEAVY_ROCK_LEGGINGS || feetArmor == HEAVY_ROCK_BOOTS){
-            if(!entity.hasStatusEffect(StatusEffects.BLINDNESS)){
-                entity.addStatusEffect(new StatusEffectInstance(StatusEffects.BLINDNESS,4));
+            if(!entity.hasStatusEffect(StatusEffects.BLINDNESS)) {
+                entity.addStatusEffect(new StatusEffectInstance(StatusEffects.BLINDNESS, 4));
                 stack.decrement(1);
+                user.playSound(SoundEvents.BLOCK_FIRE_EXTINGUISH, 1.0f, new Random().nextFloat());
             }
         }
         return true;
