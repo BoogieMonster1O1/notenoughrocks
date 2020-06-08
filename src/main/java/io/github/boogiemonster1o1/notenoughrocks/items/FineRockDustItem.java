1f7d39498a121f43401e21ca562ca4b24821cf4d
BoogieMonster1O1 <harrypotter.starwars.shrish@gmail.com>
2020-06-08 10:02:46 +0530

    Code cleanup and packet

--- src/main/java/io/github/boogiemonster1o1/notenoughrocks/items/FineRockDustItem.java
+++ src/main/java/io/github/boogiemonster1o1/notenoughrocks/items/FineRockDustItem.java
@@ -2,25 +2,32 @@ package io.github.boogiemonster1o1.notenoughrocks.items;
 
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import net.fabricmc.fabric.api.network.ServerSidePacketRegistry;
 import net.minecraft.client.item.TooltipContext;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.effect.StatusEffectInstance;
-import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.sound.SoundEvents;
 import net.minecraft.text.Text;
 import net.minecraft.text.TranslatableText;
 import net.minecraft.util.Hand;
+import net.minecraft.util.PacketByteBuf;
 import net.minecraft.util.Rarity;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
 import java.util.List;
 import java.util.Random;
+import java.util.stream.Stream;
 
 import static io.github.boogiemonster1o1.notenoughrocks.Elements.ItemS.*;
+import static io.github.boogiemonster1o1.notenoughrocks.NotEnoughRocks.PLAY_DUST_PARTICLE;
+import static io.netty.buffer.Unpooled.buffer;
+import static net.fabricmc.fabric.api.server.PlayerStream.watching;
 import static net.minecraft.entity.EquipmentSlot.*;
+import static net.minecraft.entity.effect.StatusEffects.BLINDNESS;
+import static net.minecraft.sound.SoundEvents.BLOCK_FIRE_EXTINGUISH;
 
 public class FineRockDustItem extends Item {
     public FineRockDustItem(Settings settings) {
@@ -30,7 +37,7 @@ public class FineRockDustItem extends Item {
     @Environment(EnvType.CLIENT)
     @Override
     public void appendTooltip(ItemStack stack, World world, List<Text> tooltip, TooltipContext context) {
-       tooltip.add(new TranslatableText("item.notenoughrocks.fine_dust.use_dust_tooltip"));
+        tooltip.add(new TranslatableText("item.notenoughrocks.fine_dust.use_dust_tooltip"));
     }
 
     @Override
@@ -39,11 +46,15 @@ public class FineRockDustItem extends Item {
         Item chestArmor = user.getEquippedStack(CHEST).getItem();
         Item legsArmor = user.getEquippedStack(LEGS).getItem();
         Item feetArmor = user.getEquippedStack(FEET).getItem();
-        if(headArmor == HEAVY_ROCK_HELMET || chestArmor == HEAVY_ROCK_CHESTPLATE || legsArmor == HEAVY_ROCK_LEGGINGS || feetArmor == HEAVY_ROCK_BOOTS){
-            if(!entity.hasStatusEffect(StatusEffects.BLINDNESS)) {
-                entity.addStatusEffect(new StatusEffectInstance(StatusEffects.BLINDNESS, 4));
+        if (headArmor == HEAVY_ROCK_HELMET || chestArmor == HEAVY_ROCK_CHESTPLATE || legsArmor == HEAVY_ROCK_LEGGINGS || feetArmor == HEAVY_ROCK_BOOTS) {
+            if (!entity.hasStatusEffect(BLINDNESS)) {
+                entity.addStatusEffect(new StatusEffectInstance(BLINDNESS, 8));
                 stack.decrement(1);
-                user.playSound(SoundEvents.BLOCK_FIRE_EXTINGUISH, 1.0f, new Random().nextFloat());
+                user.playSound(BLOCK_FIRE_EXTINGUISH, 1.0f, new Random().nextFloat());
+                Stream<PlayerEntity> allPlayers = watching(entity);
+                PacketByteBuf clientData = new PacketByteBuf(buffer());
+                clientData.writeBlockPos(new BlockPos(entity.getX(), entity.getY(), entity.getZ()));
+                allPlayers.forEach(player -> ServerSidePacketRegistry.INSTANCE.sendToPlayer(player, PLAY_DUST_PARTICLE, clientData));
             }
         }
         return true;
