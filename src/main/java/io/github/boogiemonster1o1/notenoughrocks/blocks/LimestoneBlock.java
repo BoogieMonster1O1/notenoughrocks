16635d010bcfb55d94ae7f323bb43dace143adac
BoogieMonster1O1 <harrypotter.starwars.shrish@gmail.com>
2020-06-13 10:18:45 +0530

    Static imports

--- src/main/java/io/github/boogiemonster1o1/notenoughrocks/blocks/LimestoneBlock.java
+++ src/main/java/io/github/boogiemonster1o1/notenoughrocks/blocks/LimestoneBlock.java
@@ -8,12 +8,13 @@ import net.minecraft.world.gen.decorator.RangeDecoratorConfig;
 import net.minecraft.world.gen.feature.OreFeatureConfig;
 
 import static io.github.boogiemonster1o1.notenoughrocks.Elements.BlockS.LIMESTONE_BLOCK;
-import static net.minecraft.world.biome.Biome.Category;
+import static net.minecraft.world.biome.Biome.Category.NETHER;
+import static net.minecraft.world.biome.Biome.Category.THEEND;
 import static net.minecraft.world.biome.Biomes.MOUNTAINS;
 import static net.minecraft.world.gen.GenerationStep.Feature.UNDERGROUND_ORES;
 import static net.minecraft.world.gen.decorator.Decorator.COUNT_RANGE;
 import static net.minecraft.world.gen.feature.Feature.ORE;
-import static net.minecraft.world.gen.feature.OreFeatureConfig.Target;
+import static net.minecraft.world.gen.feature.OreFeatureConfig.Target.NATURAL_STONE;
 
 public class LimestoneBlock extends Block {
     public LimestoneBlock() {
@@ -21,7 +22,7 @@ public class LimestoneBlock extends Block {
     }
 
     public void genLimestone(Biome biome) {
-        if ((biome.getCategory() != Category.NETHER && biome.getCategory() != Category.THEEND) && (biome != MOUNTAINS))
-            biome.addFeature(UNDERGROUND_ORES, ORE.configure(new OreFeatureConfig(Target.NATURAL_STONE, LIMESTONE_BLOCK.getDefaultState(), 28)).createDecoratedFeature(COUNT_RANGE.configure(new RangeDecoratorConfig(10, 0, 0, 156))));
+        if ((biome.getCategory() != NETHER && biome.getCategory() != THEEND) && (biome != MOUNTAINS))
+            biome.addFeature(UNDERGROUND_ORES, ORE.configure(new OreFeatureConfig(NATURAL_STONE, LIMESTONE_BLOCK.getDefaultState(), 28)).createDecoratedFeature(COUNT_RANGE.configure(new RangeDecoratorConfig(10, 0, 0, 156))));
     }
 }
