17b0edec1196403c7186273ac7f8930f80ff56ad
Shrish Deshpande <harrypotter.starwars.shrish@gmail.com>
2020-06-29 13:33:34 +0530

    Lucky stuff
     Changes to be committed:
    	modified:   src/main/java/io/github/boogiemonster1o1/notenoughrocks/blocks/CrushedLimestoneBlock.java

--- src/main/java/io/github/boogiemonster1o1/notenoughrocks/blocks/CrushedLimestoneBlock.java
+++ src/main/java/io/github/boogiemonster1o1/notenoughrocks/blocks/CrushedLimestoneBlock.java
@@ -4,9 +4,32 @@ import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.fabricmc.fabric.api.tool.attribute.v1.FabricToolTags;
 import net.minecraft.block.FallingBlock;
 import net.minecraft.block.Material;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.ItemEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
+import java.util.Random;
+
+import static io.github.boogiemonster1o1.notenoughrocks.Elements.ItemS.FINE_DUST;
 
 public class CrushedLimestoneBlock extends FallingBlock {
     public CrushedLimestoneBlock() {
         super(FabricBlockSettings.of(Material.AGGREGATE).resistance(2f).hardness(0.2f).breakByTool(FabricToolTags.SHOVELS));
+    }
+
+    @Override
+    public void onSteppedOn(World world, BlockPos pos, Entity entity) {
+        if(entity instanceof PlayerEntity){
+            Random rand = new Random();
+            int lucky = rand.nextInt(16);
+            if(lucky == 1){
+                if(!world.isClient()){
+                    world.spawnEntity(new ItemEntity(world,entity.getX(),entity.getY(),entity.getZ(),new ItemStack(FINE_DUST)));
+                }
+            }
+        }
     }
 }
